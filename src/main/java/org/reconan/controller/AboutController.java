08287554a270a9f6d60cd0e0271d6c08add458b2
Youness SBAI <youness.sbai.work@gmail.com>
2026-05-13 23:51:54 +0100

    feat: add mouse-wheel zoom and panning to About page screenshots

--- src/main/java/org/reconan/controller/AboutController.java
+++ src/main/java/org/reconan/controller/AboutController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
 import org.reconan.ui.ViewLoader;
 
 /**
@@ -17,6 +18,50 @@ public class AboutController {
     @FXML
     private ImageView zoomedImage;
 
+    private double lastMouseX;
+    private double lastMouseY;
+
+    @FXML
+    public void initialize() {
+        // Setup Zoom (Mouse Wheel)
+        zoomOverlay.addEventHandler(ScrollEvent.SCROLL, event -> {
+            if (zoomOverlay.isVisible()) {
+                double delta = event.getDeltaY();
+                double zoomFactor = 1.05;
+                if (delta < 0) zoomFactor = 2.0 - zoomFactor;
+
+                double newScaleX = zoomedImage.getScaleX() * zoomFactor;
+                double newScaleY = zoomedImage.getScaleY() * zoomFactor;
+
+                // Limit zoom
+                if (newScaleX > 0.5 && newScaleX < 10) {
+                    zoomedImage.setScaleX(newScaleX);
+                    zoomedImage.setScaleY(newScaleY);
+                }
+                event.consume();
+            }
+        });
+
+        // Setup Panning (Mouse Drag)
+        zoomedImage.setOnMousePressed(event -> {
+            lastMouseX = event.getSceneX();
+            lastMouseY = event.getSceneY();
+            event.consume();
+        });
+
+        zoomedImage.setOnMouseDragged(event -> {
+            double deltaX = event.getSceneX() - lastMouseX;
+            double deltaY = event.getSceneY() - lastMouseY;
+
+            zoomedImage.setTranslateX(zoomedImage.getTranslateX() + deltaX);
+            zoomedImage.setTranslateY(zoomedImage.getTranslateY() + deltaY);
+
+            lastMouseX = event.getSceneX();
+            lastMouseY = event.getSceneY();
+            event.consume();
+        });
+    }
+
     @FXML
     private void handleBack() {
         System.out.println("Terminal: Returning to Main Menu...");
@@ -28,6 +73,13 @@ public class AboutController {
         System.out.println("Terminal: Zooming image...");
         ImageView clickedImage = (ImageView) event.getSource();
         zoomedImage.setImage(clickedImage.getImage());
+        
+        // Reset state
+        zoomedImage.setScaleX(1.0);
+        zoomedImage.setScaleY(1.0);
+        zoomedImage.setTranslateX(0);
+        zoomedImage.setTranslateY(0);
+        
         zoomOverlay.setVisible(true);
     }
 
